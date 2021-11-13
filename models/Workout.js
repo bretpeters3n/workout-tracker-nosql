8455d2f750a65ff4be18c20367c1b696a77c16e9
Bret Petersen <bretpetersen82@gmail.com>
2021-11-13 14:41:31 -0600

    initial API routes in controller are written

--- models/Workout.js
+++ models/Workout.js
@@ -1,73 +1,60 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// {
-//   "_id" : ObjectId("618dbbca7140fe78317487fd"),
-//   "day" : ISODate("2021-11-06T23:56:42.717Z"),
-//   "exercises" : [
-//       {
-//           "type" : "resistance",
-//           "name" : "Bench Press",
-//           "duration" : 20,
-//           "weight" : 285,
-//           "reps" : 10,
-//           "sets" : 4
-//       }
-//   ]
-// }
-
-// I should also be able to track the name, type, weight, sets, reps, and
-// duration of exercise. If the exercise is a cardio exercise, I should be able
-// to track my distance traveled.
-
-// Name, Type, Weight, Sets, Reps, Duration, Distance
-
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Workout name is required",
+  day: {
+    type: Date,
   },
 
-  type: {
-    type: String,
-    trim: true,
-    required: "Workout type is required",
-  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Workout name is required",
+      },
 
-  weight: {
-    type: Number,
-    trim: true,
-    required: "Workout weight is required",
-  },
+      type: {
+        type: String,
+        trim: true,
+        required: "Workout type is required",
+      },
 
-  sets: {
-    type: Number,
-    trim: true,
-    required: "Workout sets are required",
-  },
+      weight: {
+        type: Number,
+        trim: true,
+        required: "Workout weight is required",
+      },
 
-  reps: {
-    type: Number,
-    trim: true,
-    required: "Workout reps are required",
-  },
+      sets: {
+        type: Number,
+        trim: true,
+        required: "Workout sets are required",
+      },
 
-  duration: {
-    type: Number,
-    trim: true,
-    required: "Workout duration is required",
-  },
+      reps: {
+        type: Number,
+        trim: true,
+        required: "Workout reps are required",
+      },
 
-  distance: {
-    type: Number,
-    trim: true,
-    required: "Workout distance is required",
-  },
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Workout duration is required",
+      },
 
-  // Unused examples vv
-  // boolean: Boolean,
-  // array: Array,
+      distance: {
+        type: Number,
+        trim: true,
+        required: "Workout distance is required",
+      },
+    },
+  ],
+
+  // totalDuration: {
+  //   type: Number,
+  // },
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,25 +1,76 @@
 const router = require("express").Router();
+const db = require("../models");
+
 const path = require("path");
 
 // these routes will be determined by the existing JS files in the PUBLIC folder
 
+// get workouts
 // GET /api/workouts/
+router.get("/api/workouts", (req, res) => {
+  console.log("entered GET /api/workouts");
+  db.Workout.find({}, (error, data) => {
+    data.forEach((workout) => {
+      var total = 0;
+      workout.exercises.forEach((e) => {
+        total += e.duration;
+      });
+      workout.totalDuration = total;
+    });
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
+// add an exercise
 // PUT /api/workouts/
+router.put("/api/workouts/:id", (req, res) => {
+  console.log("entered PUT /api/workouts");
+  db.Workout.findOneAndUpdate(
+    {
+      _id: mongojs.ObjectId(req.params.id),
+    },
+    {
+      $inc: { totalDuration: req.body.duration },
+      $push: { exercises: req.body },
+    },
+    { new: true },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
 
+// create a workout
 // POST /api/workouts/
-type = "cardio";
-name = cardioNameInput;
-distance = Number;
-duration = Number;
-
-type = "resistance";
-name = nameInput;
-weight = Number;
-sets = Number;
-reps = Number;
-duration = Number;
+router.post("/api/workouts", (req, res) => {
+  console.log("entered POST /api/workouts");
+  db.Workout.insert(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 // GET /api/workouts/range
+router.get("/api/workouts/range", (req, res) => {
+  console.log("entered GET /api/workouts/range");
+  db.Workout.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
 module.exports = router;
